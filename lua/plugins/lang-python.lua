2369161bae9ef55748584f8d3e45ad0956bbba54
piotr wolkowski <piotr.wolkowski@gmail.com>
2026-02-07 23:08:19 +0400

    Corrections to catpuccin color scheme 🐈🐈🐈.

--- lua/plugins/lang-python.lua
+++ lua/plugins/lang-python.lua
@@ -5,7 +5,9 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "python" })
-      -- opts.highlight = opts.highlight or {}
+      opts.highlight = opts.highlight or {}
+      opts.highlight.disable = false
+      opts.highlight.enable = true
       -- opts.highlight.disable = opts.highlight.disable or {}
       -- vim.list_extend(opts.highlight.disable, { "python" })
     end,
@@ -21,21 +23,41 @@ return {
   },
 
   -- Disable LSP semantic tokens for Pyright so syntax/colorscheme control Python highlighting
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       pyright = {
+  --         on_init = function(client)
+  --           if client.server_capabilities then
+  --             client.server_capabilities.semanticTokensProvider = nil
+  --           end
+  --         end,
+  --       },
+  --     },
+  --   },
+  -- },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
-          on_init = function(client)
-            if client.server_capabilities then
-              client.server_capabilities.semanticTokensProvider = nil
-            end
-          end,
-        },
+        pyright = {},
+      },
+      setup = {
+        pyright = function(_, opts)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client and client.name == "pyright" then
+                client.server_capabilities.semanticTokensProvider = nil
+              end
+            end,
+          })
+        end,
       },
     },
   },
-
   -- Formatters and linters for Python
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
